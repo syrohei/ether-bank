06fa7784f206dbdce04c7cd4b80ae8b991621523
syrohei <syrohei@gmail.com>
2016-12-23 00:07:38 +0900

    add readme

---
+++ contracts/User.sol
@@ -0,0 +1,81 @@
+pragma solidity ^0.4.2;
+
+
+// This is just a simple example of a coin-like contract.
+// It is not standards compatible and cannot be expected to talk to other
+// coin/token contracts. If you want to create a standards-compliant
+// token, see: https://github.com/ConsenSys/Tokens. Cheers!
+
+import "Validate.sol";
+
+contract owned {
+  function owned() {  }
+  address owner;
+
+  modifier onlyOwner {
+    if (msg.sender != owner)
+      throw;
+    _;
+  }
+}
+
+
+contract User is owned{
+  mapping (address => uint) balances;
+  mapping (address => uint) lending_balances;
+
+  Validate.Data validated_user;
+  Validate.Data locked_user;
+
+
+  function User(address _owner ){
+    owner = _owner;
+  }
+
+  function register(address _user) onlyOwner {
+    if (!Validate.insert(validated_user, _user))
+      throw;
+  }
+  function remove(address _user) onlyOwner {
+    if (!onlyUser(_user))
+      throw;
+    if (!Validate.remove(validated_user, _user))
+      throw;
+  }
+
+  function account_lock(address _user) onlyOwner {
+    if (!onlyUser(_user) || !lockable(_user))
+      throw;
+    if (!Validate.insert(locked_user, _user))
+      throw;
+  }
+
+  function send(address _receiver, uint _amount) onlyUser returns(bool sufficient) {
+    if (!onlyUser(msg.sender) || !onlyUser(_receiver)  || !lockable(msg.sender) )
+      return false;
+    if (balances[msg.sender] < _amount)
+      return false;
+
+    balances[msg.sender] -= _amount;
+    balances[_receiver] += _amount;
+    return true;
+  }
+
+  function getBalanceOfGeneral(address addr) returns(uint) {
+    return balances[addr];
+  }
+
+  function getBalanceOfLending(address addr) returns(uint) {
+    return lending_balances[addr];
+  }
+
+
+  function onlyUser(address _user) returns (bool){
+    return Validate.show(validated_user, _user);
+  }
+
+  function lockable(address _user) returns (bool){
+    return Validate.show(locked_user, _user);
+
+  }
+}
---
+++ contracts/Validate.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.0;
+
+library Validate {
+  // We define a new struct datatype that will be used to
+  // hold its data in the calling contract.
+  struct Data { mapping(address => bool) map; }
+
+  // Note that the first parameter is of type "storage
+  // reference" and thus only its storage address and not
+  // its contents is passed as part of the call.  This is a
+  // special feature of library functions.  It is idiomatic
+  // to call the first parameter 'self', if the function can
+  // be seen as a method of that object.
+  function insert(Data storage self, address value)
+      returns (bool)
+  {
+      if (self.map[value])
+        return false; // already there
+      self.map[value] = true;
+      return true;
+  }
+
+  function remove(Data storage self, address value)
+      returns (bool)
+  {
+      if (!self.map[value])
+        return false; // not there
+      self.map[value] = false;
+      return true;
+  }
+
+  function show(Data storage self, address value)
+      returns (bool)
+  {
+      if (self.map[value] != true)
+        return  false;
+
+      return true;
+  }
+
+}
