632430f892153e5a4a208a252739639ec57351db
syrohei <syrohei@gmail.com>
2016-12-23 00:44:25 +0900

    fix geth_docker

---
+++ geth_docker/geth/main_net.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+# Generate and store a wallet password
+if [ ! -f ~/.accountpassword ]; then
+  echo `date +%s | sha256sum | base64 | head -c 32` > ~/.accountpassword
+fi
+
+if [ ! -f ~/.primaryaccount ]; then
+  geth  --password ~/.accountpassword account new > ~/.primaryaccount
+fi
+
+geth --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --password ~/.accountpassword 
+
---
+++ geth_docker/geth/test_net.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+# Generate and store a wallet password
+if [ ! -f ~/.accountpassword ]; then
+  echo `date +%s | sha256sum | base64 | head -c 32` > ~/.accountpassword
+fi
+
+if [ ! -f ~/.primaryaccount ]; then
+  geth --testnet --password ~/.accountpassword account new > ~/.primaryaccount
+fi
+
+geth --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --testnet --password ~/.accountpassword --mine --minerthreads 1 --extradata "syrohei"
+
